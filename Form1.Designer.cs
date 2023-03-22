0caa85744ddded743d0c48357616180b57040a86
JAY <93957429+jaymariej@users.noreply.github.com>
2023-03-22 12:12:58 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.submitB = new System.Windows.Forms.Button();
+            this.closeB = new System.Windows.Forms.Button();
+            this.othersCheckBox = new System.Windows.Forms.CheckBox();
+            this.othersTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(24, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Survey";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(26, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(242, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Which country would you like to visit?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "If others, please specify:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(29, 104);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Japan";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(29, 140);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(76, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Philippines";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(161, 104);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(67, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Thailand";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(161, 140);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(66, 17);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "Australia";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // submitB
+            // 
+            this.submitB.Location = new System.Drawing.Point(72, 270);
+            this.submitB.Name = "submitB";
+            this.submitB.Size = new System.Drawing.Size(67, 29);
+            this.submitB.TabIndex = 8;
+            this.submitB.Text = "Submit";
+            this.submitB.UseVisualStyleBackColor = true;
+            this.submitB.Click += new System.EventHandler(this.submitB_Click);
+            // 
+            // closeB
+            // 
+            this.closeB.Location = new System.Drawing.Point(145, 270);
+            this.closeB.Name = "closeB";
+            this.closeB.Size = new System.Drawing.Size(71, 29);
+            this.closeB.TabIndex = 9;
+            this.closeB.Text = "Close";
+            this.closeB.UseVisualStyleBackColor = true;
+            this.closeB.Click += new System.EventHandler(this.closeB_Click);
+            // 
+            // othersCheckBox
+            // 
+            this.othersCheckBox.AutoSize = true;
+            this.othersCheckBox.Location = new System.Drawing.Point(83, 172);
+            this.othersCheckBox.Name = "othersCheckBox";
+            this.othersCheckBox.Size = new System.Drawing.Size(57, 17);
+            this.othersCheckBox.TabIndex = 10;
+            this.othersCheckBox.Text = "Others";
+            this.othersCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // othersTextBox
+            // 
+            this.othersTextBox.Location = new System.Drawing.Point(29, 227);
+            this.othersTextBox.Name = "othersTextBox";
+            this.othersTextBox.Size = new System.Drawing.Size(100, 20);
+            this.othersTextBox.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 320);
+            this.Controls.Add(this.othersTextBox);
+            this.Controls.Add(this.othersCheckBox);
+            this.Controls.Add(this.closeB);
+            this.Controls.Add(this.submitB);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button submitB;
+        private System.Windows.Forms.Button closeB;
+        private System.Windows.Forms.CheckBox othersCheckBox;
+        private System.Windows.Forms.TextBox othersTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void closeB_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void submitB_Click(object sender, EventArgs e)
+        {
+            string str = " ";
+
+            if (checkBox1.Checked == true)
+            {
+                str = str + checkBox1.Text;
+                str += " ";
+            }
+
+            if (checkBox2.Checked == true)
+            {
+                str = str + checkBox2.Text;
+                str += " ";
+            }
+            if (checkBox3.Checked == true)
+            {
+                str = str + checkBox3.Text;
+                str += " ";
+            }
+            if (checkBox4.Checked == true)
+            {
+                str = str + checkBox4.Text;
+                str += " ";
+            }
+            if (othersCheckBox.Checked == true)
+            {
+                str = str + othersTextBox.Text;
+                str += " ";
+            }
+            if (str != null)
+            {
+                MessageBox.Show("You Selected: \n" + str + "\nThank You!", "Survey", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+
+}
+
